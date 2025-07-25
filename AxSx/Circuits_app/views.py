242fd096c226c5c8aa889be605d5150bbd7f911b
NiceeFlight <stray.zz@gmail.com>
2025-07-26 03:14:54 +0800

    initial commit

---
+++ AxSx/Circuits_app/views.py
@@ -0,0 +1,22 @@
+import json
+from django.http import JsonResponse
+from django.shortcuts import render
+
+
+def circuits_json(request):
+    with open("static/js/f1-circuits.geojson", "r", encoding="utf-8") as file:
+        data = json.load(file)
+    # print(data)
+    # selectList = []
+    # for i in data["features"]:
+    #     name = i["properties"]["Name"]
+    #     selectList.append(name)
+
+    return JsonResponse(data=data)
+
+
+def circuits(request):
+    context = {
+        "name": "Circuits Map",
+    }
+    return render(request, "circuits.html", context)
---
+++ AxSx/Map_app/urls.py
@@ -0,0 +1,7 @@
+from Map_app import views
+from django.urls import path
+
+
+urlpatterns = [
+    path("api/views_AxSx/", views.view_AxSx, name="view_AxSx"),
+]
